1af952583d34bba00a39df5603baab127ea6675c
Rafael B. Januzi <rjanuzi@gmail.com>
2023-03-29 07:17:29 -0300

    Creating Pet model

---
+++ server/models/Pet.js
@@ -0,0 +1,10 @@
+class Pet {
+  constructor(name, age, type, breed, size, mood) {
+    this.name = name;
+    this.age = age;
+    this.type = type;
+    this.breed = breed;
+    this.size = size;
+    this.mood = mood;
+  }
+}
