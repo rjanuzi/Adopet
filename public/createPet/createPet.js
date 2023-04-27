7950cfcd8d5deeb7863f3d76e43d0f2273a7d11e
Rafael B. Januzi <rjanuzi@gmail.com>
2023-04-27 08:15:48 -0300

    Advances in update Pet feature

--- public/createPet/createPet.js
+++ public/createPet/createPet.js
@@ -1,14 +1,6 @@
-console.log("Adding event");
-
-document.getElementById("create-btn").addEventListener("click", () =>
-  createPet().then((result) => {
-    if (result) {
-      window.location.href = "/";
-    } else {
-      console.log(`Deu ruim: ${result}`);
-    }
-  })
-);
+const url = window.location.href;
+const lastUrlToken = url.split("/").pop();
+const petId = lastUrlToken == "createPet" ? "" : lastUrlToken;
 
 const inputName = document.getElementById("inputName");
 const inputAge = document.getElementById("inputAge");
@@ -16,6 +8,63 @@ const inputAnimalType = document.getElementById("inputAnimalType");
 const inputBreed = document.getElementById("inputBreed");
 const inputSize = document.getElementById("inputSize");
 const inputMood = document.getElementById("inputMood");
+const createBtn = document.getElementById("create-btn");
+const saveBtn = document.getElementById("save-btn");
+
+/* Query Pet Data and fill form */
+async function fillForm(petId) {
+  const response = await fetch("/pets/" + petId, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+  const petData = await response.json();
+
+  console.log(petData);
+
+  // TODO -- Fill form with PetData
+  inputName.value = petData.name;
+  inputAge.value = petData.age;
+  inputAnimalType.value = petData.animalType;
+  inputBreed.value = petData.breed;
+  inputSize.value = petData.size;
+  inputMood.value = petData.mood;
+}
+
+/* Configure page for create or update Pet */
+function configPage() {
+  const isCreatePage = petId == "";
+
+  if (isCreatePage) {
+    /* Configure for Create Page */
+    saveBtn.style.display = "none";
+    createBtn.addEventListener("click", () =>
+      createPet().then((result) => {
+        if (result) {
+          window.location.href = "/";
+        } else {
+          console.log(`Deu ruim: ${result}`);
+        }
+      })
+    );
+  } else {
+    /* Configure to update page */
+    createBtn.style.display = "none";
+    saveBtn.addEventListener("click", () =>
+      savePet().then((result) => {
+        if (result) {
+          window.location.href = "/";
+        } else {
+          console.log(`Deu ruim: ${result}`);
+        }
+      })
+    );
+
+    fillForm(petId);
+  }
+}
 
 function formToObject() {
   const newPetData = {
@@ -42,3 +91,21 @@ async function createPet() {
   const result = await response.json();
   return result.result;
 }
+
+async function savePet() {
+  //TODO
+  // const response = await fetch("/pets", {
+  //   method: "POST",
+  //   headers: {
+  //     Accept: "application/json",
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(formToObject()),
+  // });
+  // const result = await response.json();
+  // return result.result;
+}
+
+addEventListener("load", (event) => {
+  configPage();
+});
--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import url from "url";
 import path from "path";
-import Pet from "./models/Pet.js";
 import { petsCollection } from "./db/dbConnect.js";
+import { ObjectId } from "mongodb";
 
 const PORT = process.env.PORT || 80;
 const app = express();
@@ -14,14 +14,28 @@ app.use(express.static(publicFolder));
 /* Auto convert body to JSON */
 app.use(express.json());
 
-app.post("/pets", async (req, res) => {
+app.post("/pets:petId?", async (req, res) => {
+  // TODO - Reimplementar para o Save
   let result = await petsCollection.insertOne(req.body);
   res.send({ result: result.acknowledged });
 });
 
-app.get("/pets", async (req, res) => {
-  let pets = await petsCollection.find({}).toArray();
+app.get("/pets/:petId?", async (req, res) => {
+  const petId = req.params.petId;
+  let pets;
+  if (petId) {
+    pets = await petsCollection.find({ _id: new ObjectId(petId) }).toArray();
+    if (pets.length > 0) {
+      pets = pets[0];
+    }
+  } else {
+    pets = await petsCollection.find({}).toArray();
+  }
   res.status(200).json(pets);
+});
+
+app.get("/createPet/:petId?", (req, res) => {
+  res.sendFile(path.join(publicFolder, "createPet/createPet.html"));
 });
 
 app.listen(PORT, () => {
