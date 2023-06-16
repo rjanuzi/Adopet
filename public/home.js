d412cc3e61a6836c05f0c44083d5a17efdf87d8a
Leonardo Bueno Sossai <leosossai.ls@gmail.com>
2023-06-16 16:02:55 -0300

    login page

--- public/home.js
+++ public/home.js
@@ -1,8 +1,4 @@
-const updateBtn = document
-  .getElementById("update-page")
-  .addEventListener("click", () => {
-    getPets().then((res) => fillPetsTable(res));
-  });
+
 
 const petsCorouselDiv = document.getElementById("pets_carousel");
 
@@ -30,7 +26,7 @@ function genSlideElement(pet, active) {
                 <div class="carousel-caption d-none d-md-block">
                   <h5><a style="color: white;" href="createPet/${pet._id}"><strong>${pet.name}<strong/></a></h5>
                   <p style="color: white;"><strong>A ${pet.age} years old ${pet.size} sized ${pet.mood} ${pet.animalType} from the breed ${pet.breed}.<strong/></p>
-                </div>R
+                </div>
               </div>
             `;
   }
@@ -67,70 +63,5 @@ async function deletePet(event) {
   await updatePets();
 }
 
-function fillPetsTable(pets) {
-  const pets_table = document.getElementById("pets_table_body");
-  pets_table.innerHTML = "";
-
-  const range = document.createRange();
-
-  pets.forEach((pet) => {
-    const frag = document.createDocumentFragment();
-    const trElement = document.createElement("tr");
-
-    /* TD Elements */
-    const tdNameElement = document.createElement("td");
-    const tdAnimalTypeElement = document.createElement("td");
-    const tdAgeElement = document.createElement("td");
-    const tdBreedElement = document.createElement("td");
-    const tdSizeElement = document.createElement("td");
-    const tdMoodElement = document.createElement("td");
-    const tdEditElement = document.createElement("td");
-    const tdDeleteElement = document.createElement("td");
-
-    /* Add align class to td element */
-    tdEditElement.classList.add("text-center");
-    tdDeleteElement.classList.add("text-center");
-
-    /* Add buttons */
-    const tdEditButton = range.createContextualFragment(
-      `<a class="btn btn-primary btn-sm" href="createPet/${pet._id}">Edit</a>`
-    );
-    const tdDeleteButton = range.createContextualFragment(
-      `<button id="delete_btn_${pet._id}" petId=${pet._id} class="btn btn-danger btn-sm">Delete</button>`
-    );
-
-    /* Fill Fields */
-    tdNameElement.innerHTML = pet.name;
-    tdAnimalTypeElement.innerHTML = pet.animalType;
-    tdAgeElement.innerHTML = pet.age;
-    tdBreedElement.innerHTML = pet.breed;
-    tdSizeElement.innerHTML = pet.size;
-    tdMoodElement.innerHTML = pet.mood;
-
-    /* Append TDs into TR */
-    trElement.appendChild(tdNameElement);
-    trElement.appendChild(tdAnimalTypeElement);
-    trElement.appendChild(tdAgeElement);
-    trElement.appendChild(tdBreedElement);
-    trElement.appendChild(tdSizeElement);
-    trElement.appendChild(tdMoodElement);
-    trElement.appendChild(tdEditElement);
-    trElement.appendChild(tdDeleteElement);
-
-    /* Append Buttons into TDs */
-    tdEditElement.appendChild(tdEditButton);
-    tdDeleteElement.appendChild(tdDeleteButton);
-
-    /* Append TR into Fragment */
-    frag.appendChild(trElement);
-
-    /* Add listener for delete */
-    const tempButton = frag.getElementById(`delete_btn_${pet._id}`);
-    tempButton.addEventListener("click", deletePet);
-
-    /* Add the Fragment to the table */
-    pets_table.appendChild(frag);
-  });
-}
 
 await updatePets();
